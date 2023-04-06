a333d86b21952a6bca79cad3a760db110031a29f
work2win <worknpeace2win@gmail.com>
2023-04-06 12:57:08 -0400

    first push
    node changes

---
+++ react-hook-frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import HomeComponents from './components/HomeComponents';
+
+function App() {
+  return (
+    <div>
+      <HomeComponents />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-hook-frontend/src/components/HomeComponents.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const HomeComponents = () => {
+    return (
+        <div className = "container">
+            <h2 className="text-center"> Art Home Page</h2>
+            <table className = "table table-center table-light">
+            <thead><th></th>              
+            </thead>
+            <tbody></tbody></table>   
+            
+        </div>
+    )
+}
+
+export default HomeComponents
+
